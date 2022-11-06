9f6fb0a2b9b4d96738ad02d2a7917a208c1bc62c
Thomas Gorny <thomas.gorny@pm.me>
2022-11-06 23:05:33 +0100

    fix imports

--- backend/src/main/scala/xyz/trival/image_viewer/modules/media/persistence.scala
+++ backend/src/main/scala/xyz/trival/image_viewer/modules/media/persistence.scala
@@ -1,5 +1,8 @@
 package xyz.trival.image_viewer.modules.media.persistence
 
+import xyz.trival.image_viewer.modules.library.model.Library
+import xyz.trival.image_viewer.modules.media.model.Media
+
 trait MediaStorage:
   def saveLibraryMedia(lib: Library, media: Seq[Media]): Unit
   def deleteLibraryMedia(lib: Library, mediaIds: Seq[String]): Unit
